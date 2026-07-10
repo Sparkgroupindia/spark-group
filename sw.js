193401ef3c215ce53443d40762946b3719f6af2d
Sparkgroupindia <shrihariprinters22@gmail.com>
2026-07-10 16:39:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,61 +1,25 @@
-const CACHE_NAME = "spark-v1";
+const CACHE_NAME = "spark-group-v3";
 
-const urlsToCache = [
-
-"/spark-group/",
-
-"/spark-group/index.html",
-
-"/spark-group/manifest.json",
-
-"/spark-group/icon-192.png",
-
-"/spark-group/icon-512.png"
-
-];
-
-self.addEventListener("install",(event)=>{
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-
-.then(cache=>cache.addAll(urlsToCache))
-
-);
+self.addEventListener("install", function(event){
 
 self.skipWaiting();
 
 });
 
-self.addEventListener("activate",(event)=>{
+
+self.addEventListener("activate", function(event){
 
 event.waitUntil(
-
 clients.claim()
-
 );
 
 });
 
-self.addEventListener("fetch",(event)=>{
+
+self.addEventListener("fetch", function(event){
 
 event.respondWith(
-
 fetch(event.request)
-
-.then(response=>{
-
-return response;
-
-})
-
-.catch(()=>{
-
-return caches.match(event.request);
-
-})
-
 );
 
 });
